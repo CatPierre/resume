e31c76db2cf05b022b5cd9d650830e974b668031
Cathrine Pierre <cathrinepierre@gmail.com>
2025-06-28 12:29:37 -0400

    changed file name

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -63,7 +63,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      href: '/assets/cnayrouzResume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
